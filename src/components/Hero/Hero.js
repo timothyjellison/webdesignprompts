b506276661a5b0b96f2192584a5b7a2b8af3e138
Timothy Ellison <timothyellison85@gmail.com>
2018-04-18 07:06:56 -0400

    Add basic styles, create Hero component, write some copy

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react';
+import {Browser} from 'react-kawaii';
+import './Hero.css';
+
+class Hero extends PureComponent {
+  render() {
+    return (
+      <header className="hero">
+        <h1 className="title">Web Design Prompts</h1>
+        <p className="subtitle">Want to <a href="http://justbuildwebsites.com/" target="_blank">just build websites</a> but have no idea what to build?</p>
+      </header>
+    );
+  }
+}
+
+export default Hero;
