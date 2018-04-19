6829a0dab533647bc7c0eec157ecdf167768ddae
Timothy Ellison <timothyellison85@gmail.com>
2018-04-19 19:14:39 -0400

    Revert "add content"
    This reverts commit 604b567259f374b9534197cde2919f0aeffe2e29.

--- src/pages/content.js
+++
@@ -1,9 +0,0 @@
-export default [
-  {text: "There's probably a company you love that has a website you hate. Redesign their site, and add your designs (with explanations of your design decisions!) to your portfolio."},
-  {text: "Create a fan site and discussion board for a person or institution that you would love to build a community around."},
-  {text: "Make a minimalist blog theme, only using two colors, a couple fonts, and lots of white space."},
-  {text: "Build a website for a professional organization like a law firm. Every choice has to ooze professionalism, so express yourself in the small details."},
-  {text: "Create a gallery website for a painter or photographer. As much as you can, let the images speak for themselves."},
-  {text: "Make a great shopping cart checkout experience. All e-commerce sites need one so you might as well be baller at making them."},
-  {text: "Redesign the website of a restaurant you love. Most restaurants do a terrible job of making their menus available on the web: figure out how to do it right."}
-];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import React from "react"
 import Hero from '../components/Hero/Hero';
-import Prompts from '../components/Prompts/Prompts';
-import Footer from '../components/Footer/Footer';
-import content from './content';
 import './index.css';
 
 export default () => {
   return (
     <div>
       <Hero />
-      <Prompts content={content}/>
-      <Footer />
     </div>
   );
 }
