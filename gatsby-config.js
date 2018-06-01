23cbf6355d6a76610eaf82f5f6f9ace68e06cd9e
Timothy Ellison <timothyellison85@gmail.com>
2018-05-31 20:24:54 -0400

    Remove unnecessary CMS code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-remark`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,10 @@
-
 const path = require('path');
+
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
-const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
-return graphql(`{
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+
+  return graphql(`{
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       limit: 1000
@@ -23,18 +24,18 @@ return graphql(`{
       }
     }
   }`)
-    .then(result => {
-      if (result.errors) {
-        return Promise.reject(result.errors);
-      }
+  .then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
 
-      result.data.allMarkdownRemark.edges
-        .forEach(({ node }) => {
-          createPage({
-            path: node.frontmatter.path,
-            component: blogPostTemplate,
-            context: {} // additional data can be passed via context
-          });
+    result.data.allMarkdownRemark.edges
+      .forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.path,
+          component: blogPostTemplate,
+          context: {}
         });
-    });
+      });
+  });
 }
